dd8ce8e074324dd68d98f35ac17d3db99a8d6887
karimbipbop <karim.el.bair@estudiantat.upc.edu>
2026-05-14 13:20:35 +0200

    feat(menu): get all menus

--- src/infrastructure/drivens/persistence/pg/PgMenuRepository.ts
+++ src/infrastructure/drivens/persistence/pg/PgMenuRepository.ts
@@ -25,6 +25,7 @@ export class PgMenuRepository implements MenuRepository {
       const menus = result.rows.map((row) => this.toEntity(row));
 
       return ok(menus);
+
     } catch (error) {
       return fail("RETRIEVE_ERROR", "Failed to retrieve menus", error);
     }
